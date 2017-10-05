f20ac921fd5307d2342494f7d57fe68586504790
Sim'S <sfaysamaritan@gmail.com>
2017-10-05 14:00:32 +0100

    Ajout des fichiers

---
+++ res/script.js
@@ -0,0 +1,13 @@
+$("#password").on("keyup",function(){
+    if($(this).val())
+        $(".glyphicon-eye-open").show();
+    else
+        $(".glyphicon-eye-open").hide();
+    });
+$(".glyphicon-eye-open").mousedown(function(){
+    $("#password").attr('type','text');
+}).mouseup(function(){
+	$("#password").attr('type','password');
+}).mouseout(function(){
+	$("#password").attr('type','password');
+});
